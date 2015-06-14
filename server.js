cf264f3f9041b88ac6c7dbb266a21b64a3b99e8d
vandervidi <vandervidi@gmail.com>
2015-06-14 18:26:56 +0300

    cwd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var dao = require('./dao');
 var bodyParser = require('body-parser');
 var url = require('url');
 var app = express();
-app.use('/', express.static('./public')).listen(process.env.PORT || 3000);
+app.use(express.static(process.cwd() + '/public'));.listen(process.env.PORT || 3000);
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
