5f1d98cc54854bccd167daf1f139dac1969a50ae
vandervidi <vandervidi@gmail.com>
2015-07-04 19:19:27 +0300

    Implementing fb connect test

--- public/js/connect.js
+++ public/js/connect.js
@@ -28,30 +28,4 @@
 //
 // });
 // });
-jQuery.fbInit = function(app_id) {
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : app_id, // App ID
-            status     : true, // check login status
-            cookie     : true, // enable cookies to allow the server to access the session
-            xfbml      : true  // parse XFBML
-        });
-    };
-
-    // Load the SDK Asynchronously
-    (function(d){
-        var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-        js = d.createElement('script'); js.id = id; js.async = true;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        d.getElementsByTagName('head')[0].appendChild(js);
-    }(document));
-
-    $('<div />').attr('id','fb-root').appendTo('body');
-};
-
-$(document).ready(function(){
-    $.fbInit('1422789871382202');
-});
-      //appId: '1422789871382202',
-      
-			
+	
