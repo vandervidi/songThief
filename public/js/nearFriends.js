602bbe78361743a244c009233aeff10b38a90315
vandervidi <vandervidi@gmail.com>
2015-07-06 19:28:09 +0300

    Draggable objects are working now

--- public/js/nearFriends.js
+++ public/js/nearFriends.js
@@ -1,53 +1,74 @@
-// var map;
-// function initialize() {
-	// var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
-// 
-	// map = new google.maps.Map(document.getElementById('gMap'), {
-		// zoom : 13,
-		// center : myLatlng,
-		// draggable: false
-	// });
-// 
-	// var marker = new google.maps.Marker({
-		// position : myLatlng,
-		// map : map,
-		// optimized: false,
-   		 // zIndex: 9999
-	// });
-// }
-// 
-// google.maps.event.addDomListener(window, 'load', initialize);
+var map,
+    overlay;
+var userLocatinoOffset = 0.00008;
+//Offset to present user location at the bottom of the map instead of the center
+var mapCenter = {
+	lat : 32.0900000 + userLocatinoOffset,
+	lng : 34.8030835
+};
+//Setting user STATIC location - Shenkar college
 
-var map, overlay;
-
+/*
+ * Google Maps initalization
+ */
 function initialize() {
-    var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
-    map = new google.maps.Map(document.getElementById('gMap'), {
-        zoom : 13,
-        center : myLatlng,
-        disableDefaultUI: true,
-        draggable: false,
-        scrollwheel: false,
-        disableDoubleClickZoom: true
-    });
+	var myLatlng = new google.maps.LatLng(mapCenter.lat, mapCenter.lng);
+	map = new google.maps.Map(document.getElementById('gMap'), {
+		zoom : 21,
+		center : myLatlng,
+		disableDefaultUI : true,
+		draggable : false,
+		scrollwheel : false,
+		disableDoubleClickZoom : true
+	});
 
-    map.Overlay= new google.maps.OverlayView();
+	var overlay = new google.maps.OverlayView();
+	overlay.draw = function() {};
 
-    map.Overlay.draw = function () {};
+	overlay.onAdd = function() {
+		var projection = this.getProjection();
+		var pixel = projection.fromLatLngToContainerPixel(marker.getPosition());
+		console.log(pixel);
+		// var gMapOverlay = document.createElement("section");
+		// gMapOverlay.innerHTML = "new div";
+		// gMapOverlay.id = "gMapOverlay";
+		// this.getPanes().overlayLayer.appendChild(gMapOverlay);
+	};
+	overlay.setMap(map);
 
-    map.Overlay.onAdd=function() {
-        var gMapOverlay = document.createElement("section");
-        gMapOverlay.innerHTML = "new div";
-        gMapOverlay.id="gMapOverlay";
-        this.getPanes().overlayLayer.appendChild(gMapOverlay);
-    };
+	var marker = new google.maps.Marker({
+		position : myLatlng,
+		optimized : false,
+		map : map
+	});
 
-    var marker = new google.maps.Marker({
-        position: myLatlng, 
-        optimized: false, 
-        map:map
-    });
-
-    map.Overlay.setMap(map);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).ready(function() {
+	/*
+	 * Draggable and droppable objects - configuration
+	 */
+	$(".draggable").draggable({
+		containment : "#wrapper",
+		revert : "invalid",
+		scroll : false
+	});
+
+	$("#chestHolder").droppable({
+		accept : ".draggable",
+		activeClass : "droppableGlowingChest",
+		hoverClass : "droppableObjectOverChest",
+		drop : function(event, ui) {
+			console.log(event);
+			console.log(ui);
+			ui.draggable.css("display", "none");
+			console.log($(this));
+			$("#chestHolder").removeClass();
+			$("#chestHolder").addClass("droppableChestAfterUserDropped");
+		}
+	});
+
+});
+
